57e3aca34be9c1285943410ef9d5fc7ea10a7c8e
Gabriel-GV <gabrielg@lcg.unam.mx>
2024-04-18 13:06:49 -0600

    Se editó at-content.py durante la clase.

--- examples_functions/at-content.py
+++ examples_functions/at-content.py
@@ -1,13 +1,30 @@
-def get_at_content(dna): 
-    length = len(dna) 
-    a_count = dna.count('A') 
-    t_count = dna.count('T') 
-    at_content = (a_count + t_count) / length 
-    return at_content
+def get_at_content(dna, num):
+    dna = dna.replace('N', '')
+    length = len(dna)
+    a_count = dna.upper().count('A')
+    t_count = dna.upper().count('T')
+    at_content = (a_count + t_count) / length
+    return round(at_content, num)
 
 
-at_content = get_at_content("ATGACTGGACCA")
+num = 2
+at_content = get_at_content("ATGACTGGACCA", num)
 print("ATGACTGGACCA" + ": " + str(at_content))
-print("AT content ATGACTGGACCA  is " + str(get_at_content("ATGACTGGACCA")))
-print(get_at_content("ATGCGCGATCGATCGAATCG"))
+print("AT content ATGACTGGACCA  is " + str(get_at_content("ATGACTGGACCA", 4)))
+print(get_at_content("ATGCGCGATCGATCGAATCG", 5))
 
+# Paso de argumento por posicion
+print(get_at_content("agctacgtacgtacgatcgatcgatcattatctagcatcgatctatcagcta", 1))
+
+# Funcionan de igualmanera al asignar un valor a las varibles de cada argumento, paso de argumento por nombre
+print(get_at_content(dna="CATCATGCACGAGATCATCAGCATATACGATTATCGACTACGAT", num=7))
+print(get_at_content(num=7, dna="CATCATGCACGAGATCATCAGCATATACGATTATCGACTACGAT"))
+
+# Esto si funciona
+print(get_at_content("ACACGACTGACGACTATTATCGAGCCGATATCGACTAGCT", num=1))
+# Esto no funciona ya que los argumento sposicionales (el 2) deben ir primero que los argumentos con nombre
+print(get_at_content(dna="ATATAGGGCATCGACTATTCGATACGATCGATTACGATCGACTA", num=2))
+
+# Probando funcion get_at_content
+assert get_at_content("ATGC", 2) == 0.5
+assert get_at_content("ATGCNNNNNNNNNNN", 2) == 0.5
